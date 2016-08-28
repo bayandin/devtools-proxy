f921a5d3ca0f016c15654a042d287e28979ec0f2
Alexander Bayandin <a.bayandin@gmail.com>
2016-08-28 17:30:17 +0100

    Handle connections to several tabs

--- devtools-proxy.py
+++ devtools-proxy.py
@@ -24,6 +24,7 @@ else:
 
 CHROME_DEBUGGING_HOST = '127.0.0.1'
 CHROME_DEBUGGING_PORT = int(sys.argv[2]) if len(sys.argv) >= 3 else 12222
+DEVTOOLS_PATTERN = re.compile(r"(127\.0\.0\.1|localhost|%s):%d/" % (CHROME_DEBUGGING_HOST, CHROME_DEBUGGING_PORT))
 
 
 async def the_handler(request):
@@ -35,9 +36,14 @@ async def the_handler(request):
 
 async def ws_handler(request):
     app = request.app
-    if not app['browser_connection_task']:
+    tab_id = request.path_qs.split('/')[-1]
+    tabs = app['tabs']
+
+    if tabs.get(tab_id) is None:
+        app['tabs'][tab_id] = {}
         # https://github.com/KeepSafe/aiohttp/commit/8d9ee8b77820a2af11d7c1716f793a610afe306f
-        app['browser_connection_task'] = app.loop.create_task(ws_browser_handler(request))
+        task = app.loop.create_task(ws_browser_handler(request))
+        app['tasks'].append(task)
 
     return await ws_client_handler(request)
 
@@ -45,37 +51,46 @@ async def ws_handler(request):
 async def ws_client_handler(request):
     unprocessed_msg = None
     app = request.app
+    path_qs = request.path_qs
+    tab_id = path_qs.split('/')[-1]
+    url = "ws://%s:%d%s" % (CHROME_DEBUGGING_HOST, CHROME_DEBUGGING_PORT, path_qs)
 
     ws_client = WebSocketResponse()
     await ws_client.prepare(request)
 
     client_id = len(app['clients']) + 1
-    app['clients'][ws_client] = client_id
+    app['clients'][ws_client] = {
+        'id': client_id,
+        'tab_id': tab_id
+    }
 
     log_prefix = "[CLIENT %d]" % client_id
 
     print(log_prefix, 'CONNECTED')
+
     while True:
-        if app['browser'] is None or app['browser'].closed:
-            url = "ws://%s:%d%s" % (CHROME_DEBUGGING_HOST, CHROME_DEBUGGING_PORT, request.path)
-            app['browser'] = await app['browser_connection_session'].ws_connect(url, autoclose=False, autoping=False)
+        if app['tabs'][tab_id].get('ws') is None or app['tabs'][tab_id]['ws'].closed:
+            session = aiohttp.ClientSession(loop=app.loop)
+            app['sessions'].append(session)
+            # TODO: handle connection to non-existing tab (in case of typo in tab_id, or try to connect to old one)
+            app['tabs'][tab_id]['ws'] = await session.ws_connect(url, autoclose=False, autoping=False)
 
         if unprocessed_msg:
             data = unprocessed_msg.data
             print(log_prefix, '>>', data)
-            app['browser'].send_str(data)
+            app['tabs'][tab_id]['ws'].send_str(data)
             unprocessed_msg = None
 
         async for msg in ws_client:
             if msg.tp == MsgType.text:
-                app['active_client'] = ws_client
-                if app['browser'].closed:
+                app['tabs'][tab_id]['active_client'] = ws_client
+                if app['tabs'][tab_id]['ws'].closed:
                     unprocessed_msg = msg
                     print(log_prefix, 'RECONNECTED')
                     break
                 data = msg.data
                 print(log_prefix, '>>', data)
-                app['browser'].send_str(data)
+                app['tabs'][tab_id]['ws'].send_str(data)
             else:
                 print(log_prefix, 'DISCONNECTED')
                 return ws_client
@@ -84,38 +99,48 @@ async def ws_client_handler(request):
 async def ws_browser_handler(request):
     log_prefix = '<<'
     app = request.app
+    tab_id = request.path_qs.split('/')[-1]
 
     while True:
-        while app['browser'] is None or app['browser'].closed:
+        while app['tabs'][tab_id].get('ws') is None or app['tabs'][tab_id]['ws'].closed:
             await asyncio.sleep(0.1)
-            print('[BROWSER]', 'WAITED')
+            print("[BROWSER %s]" % tab_id, 'WAITED')
         else:
-            print('[BROWSER]', 'CONNECTED')
+            print("[BROWSER %s]" % tab_id, 'CONNECTED')
 
-        async for msg in app['browser']:
+        async for msg in app['tabs'][tab_id]['ws']:
             if msg.tp == MsgType.text:
-                client_id = app['clients'][app['active_client']]
-                print('[CLIENT %d]' % client_id, log_prefix, msg.data)
-                app['active_client'].send_str(msg.data)
+                if json.loads(msg.data).get('id') is None:
+                    clients = {k: v for k, v in app['clients'].items() if v.get('tab_id') == tab_id}
+                    for client in clients.keys():
+                        if not client.closed:
+                            client_id = app['clients'][client]['id']
+                            print('[CLIENT %d]' % client_id, log_prefix, msg.data)
+                            client.send_str(msg.data)
+                else:
+                    client_id = app['clients'][app['tabs'][tab_id]['active_client']]['id']
+                    print('[CLIENT %d]' % client_id, log_prefix, msg.data)
+                    app['tabs'][tab_id]['active_client'].send_str(msg.data)
             else:
-                print('[BROWSER]', 'DISCONNECTED')
+                print("[BROWSER %s]" % tab_id, 'DISCONNECTED')
                 break
 
 
 async def proxy_handler(request):
-    session = aiohttp.ClientSession(loop=request.app.loop)
     path_qs = request.path_qs
+    session = aiohttp.ClientSession(loop=request.app.loop)
     url = "http://%s:%s%s" % (CHROME_DEBUGGING_HOST, CHROME_DEBUGGING_PORT, path_qs)
+
+    print("[HTTP] %s" % path_qs)
     try:
         if request.path in ['/json', '/json/list']:
             response = await session.get(url)
             data = await response.json()
-            pattern = re.compile(r"(127\.0\.0\.1|localhost|%s):%d/" % (CHROME_DEBUGGING_HOST, CHROME_DEBUGGING_PORT))
             proxy_debugging_port = int(request.host.split(':')[1])
             for tab in data:
                 for k, v in tab.items():
                     if ":%d/" % CHROME_DEBUGGING_PORT in v:
-                        tab[k] = pattern.sub("%s:%d/" % (PROXY_DEBUGGING_HOST, proxy_debugging_port), tab[k])
+                        tab[k] = DEVTOOLS_PATTERN.sub("%s:%d/" % (PROXY_DEBUGGING_HOST, proxy_debugging_port), tab[k])
                 if tab.get('devtoolsFrontendUrl') is None:
                     if tab.get('id') is not None:
                         tab['devtoolsFrontendUrl'] = "/devtools/inspector.html?ws=%s:%d/devtools/page/%s" % (
@@ -126,8 +151,8 @@ async def proxy_handler(request):
             return Response(status=response.status, body=json.dumps(data).encode('utf-8'))
         else:
             return await transparent_request(session, url)
-    except (aiohttp.errors.ClientOSError, aiohttp.errors.ClientResponseError):
-        return aiohttp.web.HTTPBadGateway()
+    except (aiohttp.errors.ClientOSError, aiohttp.errors.ClientResponseError) as exc:
+        return aiohttp.web.HTTPBadGateway(text=str(exc))
     finally:
         session.close()
 
@@ -141,10 +166,11 @@ async def transparent_request(session, url):
 async def init(loop):
     app = Application(loop=loop)
     app['clients'] = {}
-    app['browser'] = None
-    app['active_client'] = None
-    app['browser_connection_task'] = None
-    app['browser_connection_session'] = aiohttp.ClientSession(loop=loop)
+    app['tabs'] = {}
+    # TODO: Move session and task handling to proper places
+    app['sessions'] = []
+    app['tasks'] = []
+
     app.router.add_route('*', '/{path:.*}', the_handler)
 
     handler = app.make_handler()
@@ -159,17 +185,16 @@ async def init(loop):
 
 
 async def finish(app, srv, handler):
-    for ws in app['clients'].keys():
+    for ws in list(app['clients'].keys()) + [tab['ws'] for tab in app['tabs'].values() if tab.get('ws') is not None]:
         if not ws.closed:
             await ws.close()
 
-    if app['browser'] and not app['browser'].closed:
-        await app['browser'].close()
+    for session in app['sessions']:
+        if not session.closed:
+            await session.close()
 
-    app['browser_connection_session'].close()
-
-    if app['browser_connection_task']:
-        app['browser_connection_task'].cancel()
+    for task in app['tasks']:
+        task.cancel()
 
     await asyncio.sleep(0.1)
     srv.close()
