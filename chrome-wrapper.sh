dfba0d77d3d0162120910c8e9dc64bd8188a01c5
Alexander Bayandin <a.bayandin@gmail.com>
2016-11-13 02:07:45 +0000

    Add --chrome-binary parameter to chrome-wrapper.sh

--- chrome-wrapper.sh
+++ chrome-wrapper.sh
@@ -1,17 +1,29 @@
 #!/usr/bin/env bash
 
-# TODO: Add parameter for real Chrome binary ( --chrome-binary=(.+) )
-# TODO: Extend find_binary function for macOS
-
 function find_binary {
+    local KERNEL_NAME=$(uname --kernel-name)
     local FILE_PATH
 
-    # https://chromium.googlesource.com/chromium/src/+/2729e442b1172c5094503a03fe356c8580bb919d/chrome/test/chromedriver/chrome/chrome_finder.cc
-    for file in "google-chrome" "chrome" "chromium" "chromium-browser"; do
-        for path in "/opt/google/chrome" "/usr/local/bin" "/usr/local/sbin" "/usr/bin" "/usr/sbin" "/bin" "/sbin"; do
+    declare -a FILES
+    declare -a PATHS
+
+    if [ ${KERNEL_NAME} == 'Linux' ]; then
+        # https://chromium.googlesource.com/chromium/src/+/2729e442b1172c5094503a03fe356c8580bb919d/chrome/test/chromedriver/chrome/chrome_finder.cc
+        FILES=(google-chrome chrome chromium chromium-browser)
+        PATHS=(/opt/google/chrome /usr/local/bin /usr/local/sbin /usr/bin /usr/sbin /bin /sbin)
+    elif [ ${KERNEL_NAME} == 'Darwin' ]; then
+        FILES=(Google\ Chrome.app/Contents/MacOS/Google\ Chrome Chromium.app/Contents/MacOS/Chromium)
+        PATHS=(/Applications)
+    else
+        echo "Unknown or unsupported OS: '${KERNEL_NAME}'" >&2
+        exit 1
+    fi
+
+    for file in "${FILES[@]}"; do
+        for path in "${PATHS[@]}"; do
             FILE_PATH="${path}/${file}"
-            if [ -e ${FILE_PATH} ]; then
-                echo -n ${FILE_PATH}
+            if [ -e "${FILE_PATH}" ]; then
+                echo -n "${FILE_PATH}"
                 return
             fi
         done
@@ -30,20 +42,23 @@ function watch_dog {
     done
 }
 
-DEV_TOOLS_PROXY_BINARY_RE="--devtools-proxy-binary=(.+)"
 PROXY_DEBUGGING_PORT_RE="--remote-debugging-port=([[:digit:]]+)"
+DEV_TOOLS_PROXY_BINARY_RE="--devtools-proxy-binary=(.+)"
 
-KNOWN_PORT=""
-KNOWN_PORT_RE="--devtools-proxy-port=([[:digit:]]+)"
+CHROME_BINARY=""
+CHROME_BINARY_RE="--chrome-binary=(.+)"
 
 CHROME_DEBUGGING_PORT=12222
 CHROME_DEBUGGING_PORT_RE="--devtools-proxy-chrome-debugging-port=([[:digit:]]+)"
 
+DEV_TOOLS_PROXY_ARGS=""
+DEV_TOOLS_PROXY_ARGS_RE="--devtools-proxy-args=(.+)"
+
 DEV_TOOLS_PROXY_LOG_FILE=/dev/null
 DEV_TOOLS_PROXY_LOG_FILE_RE="--devtools-proxy-log-file=(.+)"
 
-DEV_TOOLS_PROXY_ARGS=""
-DEV_TOOLS_PROXY_ARGS_RE="--devtools-proxy-args=(.+)"
+KNOWN_PORT=""
+KNOWN_PORT_RE="--devtools-proxy-port=([[:digit:]]+)"
 
 declare -a CLI_PARAMS=("$@")
 
@@ -55,17 +70,20 @@ for i in ${!CLI_PARAMS[@]}; do
     elif [[ ${VALUE} =~ ${DEV_TOOLS_PROXY_BINARY_RE} ]]; then
         DEV_TOOLS_PROXY_BINARY=${BASH_REMATCH[1]}
         unset CLI_PARAMS[${i}]
-    elif [[ ${VALUE} =~ ${DEV_TOOLS_PROXY_LOG_FILE_RE} ]]; then
-        DEV_TOOLS_PROXY_LOG_FILE=${BASH_REMATCH[1]}
-        unset CLI_PARAMS[${i}]
-    elif [[ ${VALUE} =~ ${KNOWN_PORT_RE} ]]; then
-        KNOWN_PORT=${BASH_REMATCH[1]}
+    elif [[ ${VALUE} =~ ${CHROME_BINARY_RE} ]]; then
+        CHROME_BINARY=${BASH_REMATCH[1]}
         unset CLI_PARAMS[${i}]
     elif [[ ${VALUE} =~ ${CHROME_DEBUGGING_PORT_RE} ]]; then
         CHROME_DEBUGGING_PORT=${BASH_REMATCH[1]}
         unset CLI_PARAMS[${i}]
     elif [[ ${VALUE} =~ ${DEV_TOOLS_PROXY_ARGS_RE} ]]; then
         DEV_TOOLS_PROXY_ARGS=${BASH_REMATCH[1]}
+        unset CLI_PARAMS[${i}]
+    elif [[ ${VALUE} =~ ${DEV_TOOLS_PROXY_LOG_FILE_RE} ]]; then
+        DEV_TOOLS_PROXY_LOG_FILE=${BASH_REMATCH[1]}
+        unset CLI_PARAMS[${i}]
+    elif [[ ${VALUE} =~ ${KNOWN_PORT_RE} ]]; then
+        KNOWN_PORT=${BASH_REMATCH[1]}
         unset CLI_PARAMS[${i}]
     fi
 done
@@ -80,14 +98,5 @@ if [ -n "$DEV_TOOLS_PROXY_BINARY" ]; then
     ( > /dev/null 2>&1 < /dev/null watch_dog ${CHROME_PID} ${PROXY_PID} & ) &
 fi
 
-KERNEL_NAME=$(uname --kernel-name)
-if [ ${KERNEL_NAME} == 'Linux' ]; then
-    CHROME_BINARY=$(find_binary)
-elif [ ${KERNEL_NAME} == 'Darwin' ]; then
-    CHROME_BINARY="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
-else
-    echo "Unknown or unsupported OS: '${KERNEL_NAME}'" >&2
-    exit 1
-fi
-
+CHROME_BINARY=${CHROME_BINARY:-$(find_binary)}
 exec -a "$0" "${CHROME_BINARY}" "${CLI_PARAMS[@]}"
