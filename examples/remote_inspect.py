2a1f900ac5fe1904ae879032356f6202b324c6da
Alexander Bayandin <a.bayandin@gmail.com>
2016-12-01 19:46:19 +0000

    Update examples
    [skip ci]

--- examples/remote_inspect.py
+++ examples/remote_inspect.py
@@ -4,9 +4,16 @@ import re
 
 import requests
 import selenium
-
 from devtools.proxy import CHROME_WRAPPER_PATH
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+def find_element(driver, locator):
+    return WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located(locator))
+
 
 if __name__ == '__main__':
     devtools_proxy_port = 9222
@@ -21,21 +28,24 @@ if __name__ == '__main__':
     }
 
     driver = selenium.webdriver.Chrome(desired_capabilities=desired_capabilities)
+    try:
+        version = requests.get('http://localhost:{}/json/version'.format(devtools_proxy_port)).json()
+        webkit_version = version['WebKit-Version']  # 537.36 (@8ee402c67ff2f8f7c746e56d3530b4dcec0709ad)
+        webkit_hash = re.search(r'\((.+)\)', webkit_version).group(1)  # @8ee402c67ff2f8f7c746e56d3530b4dcec0709ad
 
-    version = requests.get('http://localhost:{}/json/version'.format(devtools_proxy_port)).json()
-    webkit_version = version['WebKit-Version']  # 537.36 (@8ee402c67ff2f8f7c746e56d3530b4dcec0709ad)
-    webkit_hash = re.search(r'\((.+)\)', webkit_version).group(1)  # @8ee402c67ff2f8f7c746e56d3530b4dcec0709ad
+        tabs = requests.get('http://localhost:{}/json/list'.format(devtools_proxy_port)).json()
+        tab = next(tab for tab in tabs if tab.get('type') == 'page')
+        devtools_frontend_url = tab['devtoolsFrontendUrl']
+        devtools_frontend_url = re.sub(r'^/devtools/', '', devtools_frontend_url)
 
-    tabs = requests.get('http://localhost:{}/json/list'.format(devtools_proxy_port)).json()
-    tab = next(tab for tab in tabs if tab.get('type') == 'page')
-    devtools_frontend_url = tab['devtoolsFrontendUrl']
-    devtools_frontend_url = re.sub(r'^/devtools/', '', devtools_frontend_url)
+        url_template = 'https://chrome-devtools-frontend.appspot.com/serve_file/{}/{}&remoteFrontend=true'
+        url = url_template.format(webkit_hash, devtools_frontend_url)
+        print(url)
 
-    url_template = 'https://chrome-devtools-frontend.appspot.com/serve_file/{}/{}&remoteFrontend=true'
-    url = url_template.format(webkit_hash, devtools_frontend_url)
-    print(url)
-
-    driver.get('http://google.co.uk')
-    driver.find_element_by_name('q').send_keys('Something')
-
-    driver.quit()
+        driver.get('https://google.co.uk')
+        find_element(driver, (By.CSS_SELECTOR, '[name = "q"]')).send_keys('ChromeDriver')
+        find_element(driver, (By.CSS_SELECTOR, 'h3 > a')).click()  # The first one search result
+        find_element(driver, (By.XPATH, '//a[text() = "Capabilities & ChromeOptions"]')).click()
+        find_element(driver, (By.XPATH, '//h3//code[text() = "chromeOptions"]')).location_once_scrolled_into_view
+    finally:
+        driver.quit()
--- examples/throttling.py
+++ examples/throttling.py
@@ -5,7 +5,6 @@ import json
 import requests
 import selenium
 import websocket
-
 from devtools.proxy import CHROME_WRAPPER_PATH
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
@@ -22,23 +21,22 @@ if __name__ == '__main__':
     }
 
     driver = selenium.webdriver.Chrome(desired_capabilities=desired_capabilities)
+    try:
+        tabs = requests.get('http://localhost:{}/json/list'.format(devtools_proxy_port)).json()
+        tab = next(tab for tab in tabs if tab.get('type') == 'page')
+        devtools_url = tab['webSocketDebuggerUrl']
+        driver.get('https://codepen.io/bayandin/full/xRpROy/')
 
-    tabs = requests.get('http://localhost:{}/json/list'.format(devtools_proxy_port)).json()
-    tab = next(tab for tab in tabs if tab.get('type') == 'page')
-    devtools_url = tab['webSocketDebuggerUrl']
-
-    driver.get('https://codepen.io/kaycebasques/full/OXZXAg/')
-
-    ws = websocket.create_connection(devtools_url)
-    data = {
-        "method": "Emulation.setCPUThrottlingRate",
-        "params": {
-            "rate": 10,
-        },
-        "id": 0,
-    }
-    ws.send(json.dumps(data))
-    ws.recv()
-    ws.close()
-
-    driver.quit()
+        ws = websocket.create_connection(devtools_url)
+        data = {
+            "method": "Emulation.setCPUThrottlingRate",
+            "params": {
+                "rate": 10,
+            },
+            "id": 0,
+        }
+        ws.send(json.dumps(data))
+        ws.recv()
+        ws.close()
+    finally:
+        driver.quit()
