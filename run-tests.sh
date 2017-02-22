be6f27e92ce936952d88e41ae0d78c8f30f5f4a3
Alexander Bayandin <a.bayandin@gmail.com>
2017-02-22 14:00:54 +0000

    Update script for running compatibility tests

--- run-tests.sh
+++ run-tests.sh
@@ -16,7 +16,7 @@ case "$1" in
         readonly TESTS="integration"
         ;;
     compatibility)
-        readonly TESTS="compatibility/selenium"
+        readonly TESTS="compatibility/selenium/py"
         ;;
     *)
         echo >&2 "Unknown argument: $1 (functional|integration|compatibility)"
@@ -29,22 +29,21 @@ readonly CHROME_WRAPPER_PATH=${CHROME_WRAPPER_PATH:-"${PROJECT_DIR}/devtools/chr
 readonly DEVTOOLS_PROXY_PATH=${DEVTOOLS_PROXY_PATH:-"${PROJECT_DIR}/devtools/proxy.py"}
 PYTEST_OPTIONS=(-n=auto --verbose --instafail)
 
-if [[ ${TESTS} == "compatibility/selenium" ]]; then
+if [[ ${TESTS} == "compatibility/selenium/py" ]]; then
     readonly WITH_DEVTOOLS_PROXY=${WITH_DEVTOOLS_PROXY:-true}
     readonly DEVTOOLS_PROXY_PATCH=$(cat ${PROJECT_DIR}/tests/compatibility/conftest.py.patch)
-    # TODO: Remove PATCH after release of the next version of Selenium 3.0.2 or 3.1.0
-    readonly PATCH=$(cat ${PROJECT_DIR}/tests/compatibility/2936.diff) # https://github.com/SeleniumHQ/selenium/pull/2936
-    echo "$DEVTOOLS_PROXY_PATCH" | patch -N -p0 -d "${PROJECT_DIR}/tests/compatibility/selenium"
-    echo "$PATCH" | patch -N -p4 -d "${PROJECT_DIR}/tests/compatibility/selenium" py/test/selenium/webdriver/common/network.py
+    echo "$DEVTOOLS_PROXY_PATCH" | patch -N -p0 -d "${PROJECT_DIR}/tests/compatibility/selenium/py"
+    cp "${PROJECT_DIR}/tests/compatibility/getAttribute.js" "${PROJECT_DIR}/tests/compatibility/selenium/py/selenium/webdriver/remote/getAttribute.js"
+    cp "${PROJECT_DIR}/tests/compatibility/isDisplayed.js" "${PROJECT_DIR}/tests/compatibility/selenium/py/selenium/webdriver/remote/isDisplayed.js"
     PYTEST_OPTIONS+=(--driver=Chrome)
 fi
 
 py.test  ${PYTEST_OPTIONS[@]} "${PROJECT_DIR}/tests/${TESTS}/"
 EXIT_CODE=$?
 
-if [[ ${TESTS} == "compatibility/selenium" ]]; then
-    echo "$PATCH" | patch -R -N -p4 -d "${PROJECT_DIR}/tests/compatibility/selenium" py/test/selenium/webdriver/common/network.py
-    echo "$DEVTOOLS_PROXY_PATCH" | patch -R -N -p0 -d "${PROJECT_DIR}/tests/compatibility/selenium"
+if [[ ${TESTS} == "compatibility/selenium/py" ]]; then
+    echo "$DEVTOOLS_PROXY_PATCH" | patch -R -N -p0 -d "${PROJECT_DIR}/tests/compatibility/selenium/py"
+    rm "${PROJECT_DIR}/tests/compatibility/selenium/py/selenium/webdriver/remote/getAttribute.js" "${PROJECT_DIR}/tests/compatibility/selenium/py/selenium/webdriver/remote/isDisplayed.js"
 fi
 
 exit ${EXIT_CODE}
